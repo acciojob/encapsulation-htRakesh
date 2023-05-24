76a9e70e43578e9c04eacfa07f3d8ea8c525bc82
htRakesh <htrakesh98@gmail.com>
2023-05-24 16:49:23 +0530

    updated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String args[])
+    {
+      RWOnly rw=new RWOnly();
+      rw.setName("Rakesh");
+      String name=rw.getName();
+      // completed
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
+    private String name;
+   
+    public void setName(String n)
+    {
+        this.name=n;
+    }
+    public String getName()
+    {
+        return this.name;
+    }
     
 }
